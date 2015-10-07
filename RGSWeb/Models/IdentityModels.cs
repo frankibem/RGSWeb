8b09f7c43b3c037b89e1dfd03a0c8c0540cd908a
Frank Ibem <soulsolja@yahoo.com>
2015-10-07 00:14:48 -0500

    Update user list page to show only first name, last name and email of registered users.

--- RGSWeb/Models/IdentityModels.cs
+++ RGSWeb/Models/IdentityModels.cs
@@ -3,13 +3,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using System.ComponentModel.DataAnnotations;
 
 namespace RGSWeb.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
