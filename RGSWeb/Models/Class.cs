f17597e976ba7ab54372fde166a91065985af640
Frank Ibem <soulsolja@yahoo.com>
2015-10-07 14:32:37 -0500

    Added MVC controllers for Class

--- RGSWeb/Models/Class.cs
+++ RGSWeb/Models/Class.cs
@@ -1,10 +1,13 @@
-﻿namespace RGSWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RGSWeb.Models
 {
     public class Class
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Prefix { get; set; }
+        [Display(Name = "Course Number")]
         public short CourseNumber { get; set; }
         public short Section { get; set; }
         public ApplicationUser Teacher { get; set; }
